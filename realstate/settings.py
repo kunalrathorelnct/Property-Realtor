81e10a070b8284450add52af261d3caaf0f1be9f
kunalrathorelnct <kunalrathore.it@gmail.com>
2020-06-09 14:11:05 +0530

    Property Realtor

--- realstate/settings.py
+++ realstate/settings.py
@@ -145,6 +145,6 @@ MESSAGE_TAGS={
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'kunalrathorepand@gmail.com'
-EMAIL_HOST_PASSWORD = 'BittuLaddu@87180'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
